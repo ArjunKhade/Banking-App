c29ad214d80e5664e1dc1bc80583adcb894d1f9a
ArjunKhade <khadearjun@gmail.com>
2026-09-14 20:09:59 +0530

    read the configuration using @value annotation

--- accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -14,6 +14,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -26,10 +27,16 @@ import org.springframework.web.bind.annotation.*;
 )
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
-@AllArgsConstructor
 public class AccountsController {
 
-    private IAccountService accountService;
+    private final IAccountService accountService;
+
+    public AccountsController(IAccountService accountService){
+      this.accountService = accountService;
+    }
+
+    @Value("${build.version}")
+    private String buildVersion;
 
     @Operation(
             summary = "Create Account REST API",
@@ -159,6 +166,29 @@ public class AccountsController {
                     .status(HttpStatus.EXPECTATION_FAILED)
                     .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_DELETE));
         }
+    }
+
+    @Operation(
+            summary = "Fetch Build Version Details REST API",
+            description = "REST API to fetch build version."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildInfo(){
+        return  ResponseEntity.status(HttpStatus.OK).body(this.buildVersion);
     }
 
 }
